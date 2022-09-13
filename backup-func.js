7118023d404903637d81cba3c0f5372090f5b927
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-13 18:35:29 -0300

    Create backup-func.js

---
+++ backup-func.js
@@ -0,0 +1,72 @@
+
+/* module.exports = function MirrorDrive (src, dst, opts) {
+  const m = mirror(src, dst, opts)
+  m.done = async function () {
+    for await (const v of m) {
+      // No-op
+    }
+  }
+  return m
+} */
+
+async function * mirror (src, dst, { filter, dryRun = false, allOps = false } = {}) {
+  await src.ready()
+  await dst.ready()
+
+  const count = { files: 0, add: 0, remove: 0, change: 0 }
+  const deleted = new Map()
+  this.count = count
+
+  for await (const dstEntry of dst.list('/')) {
+    const { key } = dstEntry
+    const srcEntry = await src.entry(key)
+
+    const fileExists = srcEntry ? !!srcEntry.value.blob : false
+    if (!fileExists) {
+      count.remove++
+      yield { op: 'remove', key, bytesRemoved: dstEntry.value.blob.byteLength, bytesAdded: 0, count: { ...count } }
+
+      if (dryRun) deleted.set(key, true)
+      else await dst.del(key)
+    }
+  }
+
+  for await (const srcEntry of src.list('/', { filter })) {
+    const { key } = srcEntry
+    const dstEntry = deleted.has(key) ? null : await dst.entry(key)
+
+    count.files++
+
+    if (dstEntry) {
+      const srcMetadata = srcEntry.value.metadata
+      const dstMetadata = dstEntry.value.metadata
+
+      const noMetadata = !srcMetadata && !dstMetadata
+      const identicalMetadata = !!(srcMetadata && dstMetadata && alike(srcMetadata, dstMetadata))
+
+      const sameMetadata = noMetadata || identicalMetadata
+      if (sameMetadata) {
+        const sameContents = await streamEquals(src.createReadStream(key), dst.createReadStream(key))
+        if (sameContents) {
+          if (allOps) yield { op: 'equal', key, bytesRemoved: 0, bytesAdded: 0, count: { ...count } }
+          continue
+        }
+      }
+    }
+
+    if (dstEntry) {
+      count.change++
+      yield { op: 'change', key, bytesRemoved: dstEntry.value.blob.byteLength, bytesAdded: srcEntry.value.blob.byteLength, count: { ...count } }
+    } else {
+      count.add++
+      yield { op: 'add', key, bytesRemoved: 0, bytesAdded: srcEntry.value.blob.byteLength, count: { ...count } }
+    }
+
+    if (!dryRun) {
+      await pipeline(
+        src.createReadStream(key),
+        dst.createWriteStream(key, { metadata: srcEntry.value.metadata })
+      )
+    }
+  }
+}
