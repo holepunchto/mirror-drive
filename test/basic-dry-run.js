a7a5f0a7cd37c4852d49e24e21c0379584fe8fb8
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-13 20:39:34 -0300

    add dry run tests

---
+++ test/basic-dry-run.js
@@ -0,0 +1,41 @@
+const test = require('brittle')
+const { createDrives, changeDrive, sortObjects } = require('./helpers/index.js')
+const mirror = require('../index.js')
+
+test('dry run - mirror localdrive into hyperdrive', async function (t) {
+  const { local, hyper } = await createDrives(t)
+
+  const expected = await changeDrive(local)
+
+  for (let i = 0; i < 2; i++) {
+    const actual = []
+    const m = mirror(local, hyper, { dryRun: true, allOps: true })
+
+    for await (const diff of m) {
+      delete diff.count
+      actual.push(diff)
+    }
+
+    t.alike(m.count, { files: 6, add: 1, remove: 1, change: 3 })
+    t.alike(sortObjects(actual), sortObjects(expected))
+  }
+})
+
+test('dry run - mirror hyperdrive into localdrive', async function (t) {
+  const { local, hyper } = await createDrives(t)
+
+  const expected = await changeDrive(hyper)
+
+  for (let i = 0; i < 2; i++) {
+    const actual = []
+    const m = mirror(hyper, local, { dryRun: true, allOps: true })
+
+    for await (const diff of m) {
+      delete diff.count
+      actual.push(diff)
+    }
+
+    t.alike(m.count, { files: 6, add: 1, remove: 1, change: 3 })
+    t.alike(sortObjects(actual), sortObjects(expected))
+  }
+})
