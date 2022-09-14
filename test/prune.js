48e92ae45a473f6db76f0fbe8dc6b5305a7c9599
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-14 13:47:15 -0300

    add test prune - dry run

--- test/prune.js
+++ test/prune.js
@@ -17,3 +17,19 @@ test('prune basic', async function (t) {
 
   t.alike(m.count, { files: 6, add: 0, remove: 1, change: 0 })
 })
+
+test('prune - dry run', async function (t) {
+  const { local, hyper } = await createDrives(t)
+
+  await hyper.put('/new-tmp.txt', Buffer.from('same'))
+
+  const m = mirror(local, hyper, { dryRun: true })
+  const diffs = await toArray(m)
+
+  t.ok(await hyper.entry('/new-tmp.txt'))
+
+  t.is(diffs.length, 1)
+  t.alike(diffs[0], { op: 'remove', key: '/new-tmp.txt', bytesRemoved: 4, bytesAdded: 0 })
+
+  t.alike(m.count, { files: 6, add: 0, remove: 1, change: 0 })
+})
