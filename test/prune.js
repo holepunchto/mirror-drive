d327c532b42dde6c3cdb8fc689c752b15ef7debf
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-14 13:38:19 -0300

    add test prune basic

---
+++ test/prune.js
@@ -0,0 +1,19 @@
+const test = require('brittle')
+const { createDrives, toArray } = require('./helpers/index.js')
+const mirror = require('../index.js')
+
+test('prune basic', async function (t) {
+  const { local, hyper } = await createDrives(t)
+
+  await hyper.put('/new-tmp.txt', Buffer.from('same'))
+
+  const m = mirror(local, hyper)
+  const diffs = await toArray(m)
+
+  t.absent(await hyper.entry('/new-tmp.txt'))
+
+  t.is(diffs.length, 1)
+  t.alike(diffs[0], { op: 'remove', key: '/new-tmp.txt', bytesRemoved: 4, bytesAdded: 0 })
+
+  t.alike(m.count, { files: 6, add: 0, remove: 1, change: 0 })
+})
