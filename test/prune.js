d6cdbcbabf75250932e3e671406da9c98d1b22c9
Lucas Barrena <lucasbarrena98@gmail.com>
2022-09-14 15:56:11 -0300

    Update prune.js

--- test/prune.js
+++ test/prune.js
@@ -9,23 +9,33 @@ test('prune basic', async function (t) {
 
   const m = mirror(local, hyper)
   const diffs = await toArray(m)
-
-  t.absent(await hyper.entry('/new-tmp.txt'))
-
   t.is(diffs.length, 1)
   t.alike(diffs[0], { op: 'remove', key: '/new-tmp.txt', bytesRemoved: 4, bytesAdded: 0 })
+
+  t.absent(await hyper.entry('/new-tmp.txt'))
 })
 
-test('prune - dry run', async function (t) {
+test('prune dry run', async function (t) {
   const { local, hyper } = await createDrives(t)
 
   await hyper.put('/new-tmp.txt', Buffer.from('same'))
 
   const m = mirror(local, hyper, { dryRun: true })
   const diffs = await toArray(m)
-
-  t.ok(await hyper.entry('/new-tmp.txt'))
-
   t.is(diffs.length, 1)
   t.alike(diffs[0], { op: 'remove', key: '/new-tmp.txt', bytesRemoved: 4, bytesAdded: 0 })
+
+  t.ok(await hyper.entry('/new-tmp.txt'))
+})
+
+test('prune disabled basic', async function (t) {
+  const { local, hyper } = await createDrives(t)
+
+  await hyper.put('/new-tmp.txt', Buffer.from('same'))
+
+  const m = mirror(local, hyper, { prune: false })
+  const diffs = await toArray(m)
+  t.is(diffs.length, 0)
+
+  t.ok(await hyper.entry('/new-tmp.txt'))
 })
