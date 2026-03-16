008355ffbd43a600d01fe2e0cfdbbd93ba2a333c
Jan Keith Darunday <jkcdarunday@gmail.com>
2026-03-16 08:29:53 +0800

    Add test to show stage corruption

---
+++ test/stage-corruption.js
@@ -0,0 +1,119 @@
+const Corestore = require('corestore')
+const Hyperdrive = require('hyperdrive')
+const MirrorDrive = require('../')
+const LocalDrive = require('localdrive')
+const test = require('brittle')
+
+test('stage with 11 files', async function (t) {
+  const platformCorestore = new Corestore(await t.tmp(), {
+    manifestVersion: 1,
+    compat: false,
+    wait: true
+  })
+  await platformCorestore.ready()
+  t.teardown(() => platformCorestore.close())
+
+  {
+    t.comment('opening corestore session')
+    const corestore = platformCorestore.session({ writable: true })
+    await corestore.ready()
+    t.teardown(() => corestore.close())
+
+    t.comment('opening drive')
+    const drive = new Hyperdrive(corestore)
+    await drive.ready()
+    t.teardown(() => drive.close())
+
+    t.comment('setting manifest')
+    await drive.db.put('manifest', 'hello world')
+
+    t.comment('seeding source drive')
+    const src = new LocalDrive(await t.tmp(), { followExternalLinks: true })
+    for (let i = 0; i < 11; i++) await src.put(`/file${i}.txt`, Buffer.from(`hello world ${i}`))
+
+    t.comment('mirroring')
+    const mirror = new MirrorDrive(src, drive, { dedup: true, batch: true })
+    let mirrorCount = 0
+    for await (const diff of mirror) {
+      if (mirrorCount++ === 2) {
+        t.comment('closing drive during mirror')
+        await drive.close()
+      }
+    }
+
+    await corestore.close()
+  }
+
+  {
+    t.comment('reopening corestore session')
+    const corestore = platformCorestore.session({ writable: true })
+    await corestore.ready()
+    t.teardown(() => corestore.close())
+
+    t.comment('reopening drive')
+    const drive = new Hyperdrive(corestore)
+    await drive.ready()
+    t.teardown(() => drive.close())
+
+    t.comment('final length is', drive.core.length)
+    t.comment('reading manifest')
+    t.is((await drive.db.get('manifest')).value, 'hello world', 'manifest should be readable')
+  }
+})
+
+test('stage with 12 files', async function (t) {
+  const platformCorestore = new Corestore(await t.tmp(), {
+    manifestVersion: 1,
+    compat: false,
+    wait: true
+  })
+  await platformCorestore.ready()
+  t.teardown(() => platformCorestore.close())
+
+  {
+    t.comment('opening corestore session')
+    const corestore = platformCorestore.session({ writable: true })
+    await corestore.ready()
+    t.teardown(() => corestore.close())
+
+    t.comment('opening drive')
+    const drive = new Hyperdrive(corestore)
+    await drive.ready()
+    t.teardown(() => drive.close())
+
+    t.comment('setting manifest')
+    await drive.db.put('manifest', 'hello world')
+
+    t.comment('seeding source drive')
+    const src = new LocalDrive(await t.tmp(), { followExternalLinks: true })
+    for (let i = 0; i < 12; i++) await src.put(`/file${i}.txt`, Buffer.from(`hello world ${i}`))
+
+    t.comment('mirroring')
+    const mirror = new MirrorDrive(src, drive, { dedup: true, batch: true })
+    let mirrorCount = 0
+    for await (const diff of mirror) {
+      if (mirrorCount++ === 2) {
+        t.comment('closing drive during mirror')
+        await drive.close()
+      }
+    }
+
+    await corestore.close()
+  }
+
+  {
+    t.comment('reopening corestore session')
+    const corestore = platformCorestore.session({ writable: true })
+    await corestore.ready()
+    t.teardown(() => corestore.close())
+
+    t.comment('reopening drive')
+    const drive = new Hyperdrive(corestore)
+    await drive.ready()
+    t.teardown(() => drive.close())
+
+    t.comment('final length is', drive.core.length)
+    t.comment('reading manifest')
+    t.is((await drive.db.get('manifest')).value, 'hello world', 'manifest should be readable')
+  }
+})
